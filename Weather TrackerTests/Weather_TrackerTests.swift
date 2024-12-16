b529f798dd32627fb7223320ecf0c2124b68026c
alfredoperry14 <alfredoperry14@gmail.com>
2024-12-15 19:35:49 -0800

    Initial Commit

---
+++ Weather TrackerTests/Weather_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TrackerTests.swift
+//  Weather TrackerTests
+//
+//  Created by Alfredo Perry on 12/15/24.
+//
+
+import Testing
+@testable import Weather_Tracker
+
+struct Weather_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
